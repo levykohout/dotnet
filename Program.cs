4aa6ce9a9feaa0877a7362b048411924c4d15f12
Levy Kohout <levykohout@users.noreply.github.com>
2016-11-21 20:35:26 -0600

    more learning c#

--- Program.cs
+++ Program.cs
@@ -15,7 +15,7 @@ namespace ConsoleApplication
         {
             CarType myCarType = CarType.Toyota;
             //defining variables
-            
+
 //             int myInt = 1;
 // float myFloat = 1f;
 // bool myBoolean = true;
@@ -29,9 +29,43 @@ int[] nums = new int[10];
 int[] nums = { 1, 2, 3, 4, 5 };
 //defining a list
 List<int> numbers = new List<int>();
-//inserting numbers in the listnumbers.Add(1);
+//inserting numbers in the list
+numbers.Add(1);
 numbers.Add(2);
 numbers.Add(3);
+
+//adding whole array to a list
+List<int> numbers = new List<int>();
+int[] array = new int[] { 1, 2, 3 };
+numbers.AddRange(array);
+
+//removing from a list
+List<string> fruits = new List<string>();
+// add fruits
+fruits.Add("apple");
+fruits.Add("banana");
+fruits.Add("orange");
+
+// now remove the banana
+fruits.Remove("banana");
+Console.WriteLine(fruits.Count);
+
+//using index
+// now remove the banana
+fruits.RemoveAt(1);
+Console.WriteLine(fruits.Count);
+
+//concatenating lists
+List<string> food = new List<string>();
+food.Add("apple");
+food.Add("banana");
+
+List<string> vegetables = new List<string>();
+vegetables.Add("tomato");
+vegetables.Add("carrot");
+
+food.AddRange(vegetables);
+Console.WriteLine(food.Count);
 
             string productName ="TV";
        int productYear = 2012;
